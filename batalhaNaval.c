c88f0482abebfa410760bf1b6314a63ed33757c2
Rafcolomby <rafcolomby@hotmail.com>
2025-05-18 23:22:11 -0300

    batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -11,7 +11,7 @@
 void exibirTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
     for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
         for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
-            printf("%d ", tabuleiro[i][j]);
+            printf("%d ", tabuleiro[i][j]); 
         }
         printf("\n");
     }
